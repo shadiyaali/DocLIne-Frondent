d649e5e796f446e3b8e4be8ab0285ee13d2e2b25
Shadiya <shadiyackn@gmail.com>
2023-07-20 20:36:03 +0530

    newss

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://www.docline.online/'
+export const BASE_URL = 'https://doc-line-frondent-te8x.vercel.app/'
 // export const BASE_URL = 'http://127.0.0.1:8000/'
 
 export const mapbox_access_token = 'pk.eyJ1IjoibGFzZXJsYWQiLCJhIjoiY2xnaXoweTBpMTBuZTNkcGh6YjJ6emh6aiJ9.fzEafvzlOKjLZPcreOKjDg'
